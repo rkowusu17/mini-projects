39f09a63b3b4e178865e92ebc7a04eb152c19de2
Reginald Owusu <reginaldmiles45@gmail.com>
2024-07-02 22:57:22 +0000

    stop_watch_touched

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./build/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        blink: {
+          " 0%": {
+            transform: "scale(1)",
+          },
+          " 50%": {
+            transform: "scale(1.005)",
+            border: "3px #d72a76 solid",
+          },
+          "100%": {
+            transform: "scale(1)",
+          },
+        },
+      },
+      animation: {
+        blink: "blink 1s ease-in-out infinite forwards",
+      },
+    },
   },
   plugins: [],
 };

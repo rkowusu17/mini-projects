43b1c03160ee66efce0d399162539884e665fe58
Reginald Owusu <reginaldmiles45@gmail.com>
2024-10-31 22:57:43 +0000

    Added new features

--- Random color/main.js
+++ Random color/main.js
@@ -1,66 +1,86 @@
 const containerEl = document.querySelector(".container");
+const inputEl = document.getElementById("number");
+const runBtn = document.getElementById("run");
+const output = document.getElementById("outcome");
+// const deleteBtn = document.getElementById("delete");
 
-for (let index = 0; index < 24; index++) {
-  const colorContainerEl = document.createElement(`div`);
-  colorContainerEl.classList.add("color-container");
+// deleteBtn.addEventListener("click", () => {
 
-  const colorCodeEl = document.createElement(`span`);
-  colorCodeEl.classList.add("color-code");
+// });
 
-  colorContainerEl.appendChild(colorCodeEl);
-  const copyBtn = document.createElement("button");
-  copyBtn.innerText = "Copy";
-  colorContainerEl.appendChild(copyBtn);
+runBtn.addEventListener("click", () => {
+  containerEl.textContent = "";
+  containerEl.style.backgroundColor = "white";
 
-  containerEl.appendChild(colorContainerEl);
-}
+  const num = inputEl.value;
+  let numInt = parseInt(num);
+  console.log(numInt);
 
-function randomColor() {
-  const chars = "0123456789BCDEF";
-  const colorCodeLength = 6;
+  // function show() {
+  // console.log(numInt);
+  for (let index = 0; index < numInt; index++) {
+    const colorContainerEl = document.createElement(`div`);
+    colorContainerEl.classList.add("color-container");
 
-  let colorCode = "";
+    const colorCodeEl = document.createElement(`span`);
+    colorCodeEl.classList.add("color-code");
 
-  for (let index = 0; index < colorCodeLength; index++) {
-    const randomNumber = Math.floor(Math.random() * chars.length);
-    colorCode += chars.substring(randomNumber, randomNumber + 1); // Substring will give you the sub of a string indexing from (a,b)
+    colorContainerEl.appendChild(colorCodeEl);
+    const copyBtn = document.createElement("button");
+    copyBtn.innerText = "Copy";
+    colorContainerEl.appendChild(copyBtn);
+
+    containerEl.appendChild(colorContainerEl);
   }
-  return colorCode;
-}
 
-const mainColorContainerEls = document.querySelectorAll(".color-container");
+  function randomColor() {
+    const chars = "0123456789BCDEF";
+    const colorCodeLength = 6;
 
-function generateColor() {
-  for (let i = 0; i < mainColorContainerEls.length; i++) {
-    const colorContainerEl = mainColorContainerEls[i];
-    const newcolorCode = randomColor();
+    let colorCode = "";
+
+    for (let index = 0; index < colorCodeLength; index++) {
+      const randomNumber = Math.floor(Math.random() * chars.length);
+      colorCode += chars.substring(randomNumber, randomNumber + 1); // Substring will give you the sub of a string indexing from (a,b)
+    }
+    return colorCode;
+  }
+
+  const mainColorContainerEls = document.querySelectorAll(".color-container");
+
+  function generateColor() {
+    for (let i = 0; i < mainColorContainerEls.length; i++) {
+      const colorContainerEl = mainColorContainerEls[i];
+      const newcolorCode = randomColor();
+      const colorCodeEl = colorContainerEl.querySelector(".color-code");
+
+      colorContainerEl.style.backgroundColor = "#" + newcolorCode;
+
+      colorCodeEl.innerText = "#" + newcolorCode;
+    }
+    numInt = 0;
+  }
+
+  generateColor();
+
+  mainColorContainerEls.forEach((colorContainerEl) => {
+    const copyBtn = colorContainerEl.querySelector("button");
     const colorCodeEl = colorContainerEl.querySelector(".color-code");
 
-    colorContainerEl.style.backgroundColor = "#" + newcolorCode;
-
-    colorCodeEl.innerText = "#" + newcolorCode;
-  }
-}
-
-generateColor();
-
-mainColorContainerEls.forEach((colorContainerEl) => {
-  const copyBtn = colorContainerEl.querySelector("button");
-  const colorCodeEl = colorContainerEl.querySelector(".color-code");
-
-  copyBtn.addEventListener("click", () => {
-    const colorCode = colorCodeEl.innerText;
-    copyClipBoard(colorCode);
-  });
-});
-
-function copyClipBoard(text) {
-  navigator.clipboard
-    .writeText(text)
-    .then(() => {
-      alert(`Color code ${text} has been copied to clipboard`);
-    })
-    .catch((error) => {
-      console.log("failed to copy to clipboard", error);
+    copyBtn.addEventListener("click", () => {
+      const colorCode = colorCodeEl.innerText;
+      copyClipBoard(colorCode);
     });
-}
+  });
+
+  function copyClipBoard(text) {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        alert(`Color code ${text} has been copied to clipboard`);
+      })
+      .catch((error) => {
+        console.log("failed to copy to clipboard", error);
+      });
+  }
+});

c75697628e66a9dd136554440fa4adaf66889bbe
Reginald Owusu <reginaldmiles45@gmail.com>
2024-11-19 00:04:48 +0000

    Added cc data

---
+++ credit card checker/apps.js
@@ -0,0 +1,54 @@
+const area = document.querySelector("main");
+const checkBtn = document.getElementById("checker");
+
+const ccData = [
+  (Card1 = {
+    number: 1002003145031,
+    name: "Reggie",
+    validity: true,
+    balance: 312,
+    ccType: "Debit",
+  }),
+  (Card2 = {
+    number: 1002003145021,
+    name: "Joel",
+    validity: true,
+    balance: 312,
+    ccType: "Debit",
+  }),
+  (Card3 = {
+    number: 1002003145032,
+    name: "Emmanuel",
+    validity: false,
+    balance: 312,
+    ccType: "Debit",
+  }),
+  (Card4 = {
+    number: 1002003145033,
+    name: "Reggie",
+    validity: true,
+    balance: 312,
+    ccType: "Debit",
+  }),
+  (Card5 = {
+    number: 1002003145034,
+    name: "Reggie",
+    validity: true,
+    balance: 312,
+    ccType: "Debit",
+  }),
+  (Card6 = {
+    number: 1002003145022,
+    name: "Reggie",
+    validity: true,
+    balance: 312,
+    ccType: "Debit",
+  }),
+  (Card7 = {
+    number: 1002003145030,
+    name: "Reggie",
+    validity: true,
+    balance: 312,
+    ccType: "Debit",
+  }),
+];
